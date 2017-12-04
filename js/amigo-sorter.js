360149fc4f623c07bed633be923d60e2f79a32c4
amigodev <ipzpnet@gmail.com>
2017-12-04 17:11:33 +0200

    Update amigo-sorter.js

--- js/amigo-sorter.js
+++ js/amigo-sorter.js
@@ -20,7 +20,6 @@ $.fn.amigoSorter = function(options) {
 	$(document.body).append( $.fn.amigoSorter.li_helper( settings.li_helper ) ); 
 
 	$(document).on(mouse_up_events, function(e) {
-		e.preventDefault(); 
 		action = false;
 		$ul.attr('data-action', false);
 		$ul.find('li').removeClass(settings.li_empty);
